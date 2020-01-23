0f8b14bb44f73f9d843dabe12a9e14bcc0597790
Michael Bäcker <michael.baecker@arbeitsagentur.de>
2020-01-23 08:52:22 +0100

    Modlist Maker auf Basis von PHP und manifest.json

---
+++ make_modlist.php
@@ -0,0 +1,32 @@
+<?php
+
+$manifestfile=file_get_contents("./manifest.json");
+
+$manifestarray=json_decode($manifestfile);
+
+// Hier muss eine Leerzeile kommen, sonst funktionier die Modliste nicht
+print "\n";
+print "| Modname | ModUrl | Summary | Author | \n";
+print "| ------- | ------ | ------- | ------ | \n";
+# print_r($manifestarray);
+foreach ($manifestarray->files as $k => $v)
+{
+    $projectid=$v->projectID;
+    $fileid=$v->fileID;
+
+    $projectjson=json_decode(file_get_contents("https://addons-ecs.forgesvc.net/api/v2/addon/".$projectid));
+
+    $thisauthor=[];
+    foreach( $projectjson->authors as $k => $v)
+    {
+        $thisauthor[]=$v->name;
+    }
+    $modname=$projectjson->name;
+    $websiteUrl=$projectjson->websiteUrl;
+    $summary=$projectjson->summary;
+    print "| ".$modname." | ";
+    print $websiteUrl." | ";
+    print $summary." | ";
+    print implode(", ", $thisauthor);
+    print " |\n";
+}
