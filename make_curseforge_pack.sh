9e35fc96e0fe0fbc9b5b361b9a6e8a548bd99717
Michael Bäcker <michael.baecker@arbeitsagentur.de>
2020-01-23 08:49:37 +0100

    Tuning des Curseforge ModPack Maker Skriptes

--- make_curseforge_pack.sh
+++ make_curseforge_pack.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 VERSION=0.2.0
+PROJECTNAME=ProjectRETv12
 
-mv mods/OptiFine* oldmods/
-~/bin/twitch-export-builder_linux_amd64 -c .build.json -d . -o ~/Downloads -p $VERSION -n ProjectRETv12
-zip -r ~/Downloads/ProjectRETv12-Server-$VERSION.zip \
+if [ ! -z "$1" ]; then
+    VERSION=$1
+fi
+
+# Temporär Entwicklungs Mods und Optifine entfernen
+mkdir -p modtemp.$$
+mv -v mods/OptiFine* modtemp.$$
+[ -f mods/externaltweaker-* ] && mv -v mods/externaltweaker-* modtemp.$$
+[ -f mods/World-Stripper-* ] && mv -v mods/World-Stripper-* modtemp.$$
+[ -f mods/craftdumper-* ] && mv -v mods/craftdumper-* modtemp.$$
+[ -f mods/worldedit-forge-* ] && mv -v mods/worldedit-forge-* modtemp.$$
+
+# Make Curseforge Modpack
+~/bin/twitch-export-builder_linux_amd64 -c .build.json -d . -o ~/Downloads -p $VERSION -n $PROJECTNAME
+
+# Make Curseforge Server Modpack
+zip -r ~/Downloads/$PROJECTNAME-Server-$VERSION.zip \
     config \
     forge-*installer.jar \
     Install.* \
@@ -15,4 +30,10 @@ zip -r ~/Downloads/ProjectRETv12-Server-$VERSION.zip \
     ServerStart.* \
     settings.*
 
-mv oldmods/OptiFine* mods/
+mv -v modtemp.$$/* mods/
+rmdir modtemp.$$
+
+unzip ~/Downloads/$PROJECTNAME-$VERSION.zip manifest.json
+
+
+# rm -v manifest.json
