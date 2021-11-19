c7f8800a61346dc217e3682a7f939531b13203b3
BakermanLP <bakermanlp@gmail.com>
2021-11-19 10:19:35 +0100

    Server Settings angepasst

--- settings.sh
+++ settings.sh
@@ -1,8 +1,8 @@
 # Don't edit these values unless you know what you are doing.
-export INSTALL_JAR="forge-1.12.2-14.23.5.2847-installer.jar"
-export SERVER_JAR="forge-1.12.2-14.23.5.2847-universal.jar"
+export INSTALL_JAR="forge-1.12.2-14.23.5.2855-installer.jar*"
+export SERVER_JAR="forge-1.12.2-14.23.5.2855-universal.jar*"
 
 # You can edit these values if you wish.
-export MIN_RAM="1024M"
-export MAX_RAM="4096M"
+export MIN_RAM="4G"
+export MAX_RAM="6G"
 export JAVA_PARAMETERS="-XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dfml.readTimeout=180"
